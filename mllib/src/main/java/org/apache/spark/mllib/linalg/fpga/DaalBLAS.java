df52bab0768b9523c34fa7feff23e86a75237849
Zhankun Tang <zhankun.tang@intel.com>
2017-06-03 23:04:05 +0800

    add timestamp for compute gemm; add thread option for DAAL

--- mllib/src/main/java/org/apache/spark/mllib/linalg/fpga/DaalBLAS.java
+++ mllib/src/main/java/org/apache/spark/mllib/linalg/fpga/DaalBLAS.java
@@ -6,30 +6,54 @@ import com.intel.daal.algorithms.gemm.*;
 import com.intel.daal.services.DaalContext;
 import com.intel.daal.services.Environment;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public class DaalBLAS {
 
   //private DaalContext context = new DaalContext();
   //private Batch gemmAlgorithm = new Batch(context, java.lang.Double.class, Method.defaultDense);
-
+  private String mode;
+  private int threadCount = 1;
+  private String modeName;
   public DaalBLAS(){
-    String mode = System.getenv("DAAL_MODE");
+    String tc = System.getenv("DAAL_TC");
+    if (tc != null && !tc.isEmpty()) {
+      this.threadCount = Integer.parseInt(tc);
+      Environment.setNumberOfThreads(this.threadCount);
+      System.out.println("[DAALgemm] thread count:" + this.threadCount);
+    } else {
+      Environment.setNumberOfThreads(Environment.getNumberOfThreads());
+    }
+    this.mode = System.getenv("DAAL_MODE");
     if ("0".equals(mode)) {
+      //this.modeName = "useCpu";
       Environment.setAcceleratorMode( Environment.AcceleratorMode.useCpu);
     } else if ("1".equals(mode)) {
+      //this.modeName = "useFpgaBalanced";
       Environment.setAcceleratorMode( Environment.AcceleratorMode.useFpgaBalanced);
     } else if ("2".equals(mode)){
+      //this.modeName = "useFpgaMax";
       Environment.setAcceleratorMode( Environment.AcceleratorMode.useFpgaMax);
     } else {
+      //this.modeName = "useCpu";
       Environment.setAcceleratorMode( Environment.AcceleratorMode.useCpu);
     }
 
   }
 
   public double[] dgemm(Batch gemmAlgorithm){
+    long threadId = Thread.currentThread().getId();
+    long start_timestamp = System.currentTimeMillis();
     Result result = gemmAlgorithm.compute();
+    long end_compute_timestamp = System.currentTimeMillis();
     HomogenNumericTable cMatrix = (HomogenNumericTable)result.get(ResultId.cMatrix);
+    long end_get_timestamp = System.currentTimeMillis();
+    System.out.println("[DAALgemm] TID:" + threadId + ":mode:" + mode +
+        ":compute:" + (end_compute_timestamp - start_timestamp) +
+        ":getresult:" + (end_get_timestamp - end_compute_timestamp) +
+        ":modeName:" + this.modeName
+    );
     return cMatrix.getDoubleArray();
   }
 
